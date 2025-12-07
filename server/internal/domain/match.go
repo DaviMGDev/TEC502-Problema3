aa5fd942d9a201a2201ef4f9be8c37bed1eb0ae1
Davi Macêdo Gomes <dev.davi.macedo.gomes@gmail.com>
2025-12-07 12:41:45 +0000

    cleanup

--- server/internal/domain/match.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Match struct {
-	RoomID string    `json:"room_id"`
-	Plays  [][2]map[string]*Card `json:"plays"`
-	Winner string    `json:"winner"`
-}
--- server/internal/domain/package.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type Package struct {
-	ID    string   `json:"id"`
-	Cards []*Card `json:"cards"`
-}
